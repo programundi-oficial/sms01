5098615ef13c203d72cf6dc8e82e27430f015eab
unknown <rodrigo@sicapsolucoes.com.br>
2018-10-19 15:49:48 -0300

    001

--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,18 @@
+
 var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+
     // deviceready Event Handler
     //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        this.receivedEvent('deviceready');
     },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -29,108 +25,70 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-function teste_click(){
-	alert("aquiiiii");
-}
+/*var app = {
+sendSms: function() {
+	var number = document.getElementById('numberTxt').value.toString(); 
+	var message = document.getElementById('messageTxt').value;
+	console.log("number=" + number + ", message= " + message);
 
-var id = 1, dialog;
-
-callback = function () {
-	cordova.plugins.notification.local.getIds(function (ids) {
-		showToast('IDs: ' + ids.join(' ,'));
-	});
-};
-
-showToast = function (text) {
-	setTimeout(function () {
-		if (device.platform != 'windows') {
-			window.plugins.toast.showShortBottom(text);
-		} else {
-			showDialog(text);
+	//CONFIGURATION
+	var options = {
+		replaceLineBreaks: false, // true to replace \n by a new line, false by default
+		android: {
+			intent: 'INTENT'  // send SMS with the native android SMS messaging
+			//intent: '' // send SMS without open any other app
 		}
-	}, 100);
-};
+	};
 
-showDialog = function (text) {
-	if (dialog) {
-		dialog.content = text;
-		return;
-	}
+	var success = function () { alert('Message sent successfully'); };
+	var error = function (e) { alert('Message Failed:' + e); };
+	sms.send(number, message, options, success, error);
+}
+};*/
+var app = {
+checkSMSPermission: function() {
+	var success = function (hasPermission) { 
+		if (hasPermission) {
+			sendSms: function() {
+			var number = document.getElementById('numberTxt').value.toString(); 
+			var message = document.getElementById('messageTxt').value;
+			alert("number=" + number + ", message= " + message);
 
-	dialog = new Windows.UI.Popups.MessageDialog(text);
+			//CONFIGURATION
+			var options = {
+				replaceLineBreaks: false, // true to replace \n by a new line, false by default
+				android: {
+					intent: 'INTENT'  // send SMS with the native android SMS messaging
+					//intent: '' // send SMS without open any other app
+				}
+			};
 
-	dialog.showAsync().done(function () {
-		dialog = null;
-	});
-};
-
- hasPermission = function () {
-cordova.plugins.notification.local.hasPermission(function (granted) {
-	showToast(granted ? 'Yes' : 'No');
-});
-};
-
-registerPermission = function () {
-cordova.plugins.notification.local.registerPermission(function (granted) {
-	showToast(granted ? 'Yes' : 'No');
-});
-};
-
-schedule = function () {
-cordova.plugins.notification.local.schedule({
-	id: 1,
-	text: 'Test Message 1',
-	icon: 'http://3.bp.blogspot.com/-Qdsy-GpempY/UU_BN9LTqSI/AAAAAAAAAMA/LkwLW2yNBJ4/s1600/supersu.png',
-	smallIcon: 'res://cordova',											
-	badge: 1,
-	data: { test: id }
-});
-};
-
-scheduleMultiple = function () {
-cordova.plugins.notification.local.schedule([{
-	id: 1,
-	text: 'Multi Message 1',
-	icon: 'res://cordova'
-}, {
-	id: 2,
-	text: 'Multi Message 2',
-	icon: 'res://icon',
-	smallIcon: 'ic_media_play'
-}, {
-	id: 3,
-	text: 'Multi Message 3',
-	icon: 'res://icon',
-	smallIcon: 'ic_media_pause'
-}]);
-};
-
-scheduleDelayed = function () {
-var now = new Date().getTime(),
-	_5_sec_from_now = new Date(now + 5 * 1000);
-
-var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
-
-cordova.plugins.notification.local.schedule({
-	id: 1,
-	title: 'Scheduled with delay',
-	text: 'Test Message 1',
-	at: _5_sec_from_now,
-	sound: sound,
-	badge: 12
-});
-};
-
-scheduleMinutely = function () {
-var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
-
-cordova.plugins.notification.local.schedule({
-	id: 1,
-	text: 'Scheduled every minute',
-	every: 'minute',
-	sound: sound,
-	icon: 'res://icon',
-	smallIcon: 'res://ic_popup_sync'
-});
+			var success = function () { alert('Message sent successfully'); };
+			var error = function (e) { alert('Message Failed:' + e); };
+			sms.send(number, message, options, success, error);
+		}
+		}
+		else {
+			// show a helpful message to explain why you need to require the permission to send a SMS
+			// read http://developer.android.com/training/permissions/requesting.html#explain for more best practices
+		}
+	};
+	var error = function (e) { alert('Something went wrong:' + e); };
+	sms.hasPermission(success, error);
+},
+requestSMSPermission: function() {
+	var success = function (hasPermission) { 
+		if (!hasPermission) {
+			sms.requestPermission(function() {
+				alert('[OK] Permission accepted')
+			}, function(error) {
+				alert('[WARN] Permission not accepted')
+				// Handle permission not accepted
+			})
+		}
+	};
+	var error = function (e) { alert('Something went wrong:' + e); };
+	sms.hasPermission(success, error);
+}
 };
 app.initialize();
