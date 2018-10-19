0de1d6118ac3946fc7d106751c2d0773ebd2aed4
unknown <rodrigo@sicapsolucoes.com.br>
2018-10-19 16:25:40 -0300

    v77

--- www/js/index.js
+++ www/js/index.js
@@ -32,6 +32,7 @@ var app = {
 function teste_click(){
 	alert("aquiiiii");
 }
+/*
 var app = {
 sendSms: function() {
 	var number = document.getElementById('numberTxt').value.toString(); 
@@ -52,51 +53,35 @@ sendSms: function() {
 	sms.send(number, message, options, success, error);
 }
 };
-/*
+*/
 var app = {
-checkSMSPermission: function() {
-	var success = function (hasPermission) { 
-		if (hasPermission) {
-			sendSms: function() {
-			var number = document.getElementById('numberTxt').value.toString(); 
-			var message = document.getElementById('messageTxt').value;
-			alert("number=" + number + ", message= " + message);
+    checkSMSPermission: function() {
+        var success = function (hasPermission) { 
+            if (hasPermission) {
+                alert("aqui enviar");
+            }
+            else {
+                // show a helpful message to explain why you need to require the permission to send a SMS
+                // read http://developer.android.com/training/permissions/requesting.html#explain for more best practices
+            }
+        };
+        var error = function (e) { alert('Something went wrong:' + e); };
+        sms.hasPermission(success, error);
+    },
+    requestSMSPermission: function() {
+        var success = function (hasPermission) { 
+            if (!hasPermission) {
+                sms.requestPermission(function() {
+                    alert('[OK] Permission accepted')
+                }, function(error) {
+                    alert('[WARN] Permission not accepted')
+                    // Handle permission not accepted
+                })
+            }
+        };
+        var error = function (e) { alert('Something went wrong:' + e); };
+        sms.hasPermission(success, error);
+    }
+};
 
-			//CONFIGURATION
-			var options = {
-				replaceLineBreaks: false, // true to replace \n by a new line, false by default
-				android: {
-					intent: 'INTENT'  // send SMS with the native android SMS messaging
-					//intent: '' // send SMS without open any other app
-				}
-			};
-
-			var success = function () { alert('Message sent successfully'); };
-			var error = function (e) { alert('Message Failed:' + e); };
-			sms.send(number, message, options, success, error);
-		}
-		}
-		else {
-			// show a helpful message to explain why you need to require the permission to send a SMS
-			// read http://developer.android.com/training/permissions/requesting.html#explain for more best practices
-		}
-	};
-	var error = function (e) { alert('Something went wrong:' + e); };
-	sms.hasPermission(success, error);
-},
-requestSMSPermission: function() {
-	var success = function (hasPermission) { 
-		if (!hasPermission) {
-			sms.requestPermission(function() {
-				alert('[OK] Permission accepted')
-			}, function(error) {
-				alert('[WARN] Permission not accepted')
-				// Handle permission not accepted
-			})
-		}
-	};
-	var error = function (e) { alert('Something went wrong:' + e); };
-	sms.hasPermission(success, error);
-}
-};*/
 app.initialize();
