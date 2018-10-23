7e48620d1da8e31c72e0fc3f5d805e46a08e9486
unknown <rodrigo@sicapsolucoes.com.br>
2018-10-23 08:57:48 -0300

    version001

---
+++ www/js/index.js
@@ -0,0 +1,39 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+function teste_rodando(){
+	alert("antes rodar");
+	teste = cordova.plugins.backgroundMode.isActive();
+	alert("rodando "+teste);	
+}
+function teste_ativar(){
+	alert("antes ativado");
+	cordova.plugins.backgroundMode.setEnabled(true);
+	alert("ativado");	
+}
+
+app.initialize();
