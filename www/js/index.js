724799b3f04d877f02f339282247d4d5e1213e95
unknown <rodrigo@sicapsolucoes.com.br>
2018-10-23 09:29:06 -0300

    version003

--- www/js/index.js
+++ www/js/index.js
@@ -53,4 +53,5 @@ function add_lista(){
 	$("#lista_teste").append("<tr><td>rodando</td><td>"+data_atual()+"</td></tr>");
 	setTimeout(add_lista,5000);
 }
+setTimeout(add_lista,5000);
 app.initialize();
