831d45c331541d32f4f3536cb0c504d787ff5847
unknown <rodrigo@sicapsolucoes.com.br>
2019-02-04 14:49:39 -0300

    edu014

--- www/login.js
+++ www/login.js
@@ -17,8 +17,7 @@ function login() {
 
 	$("#btn_login").attr("disabled", true);
 	
-	console.log("y:"+y);
-	
+		
 	$.ajax({            
 		url: xurlq,
 		 data: {
@@ -27,12 +26,12 @@ function login() {
 			p1: p1,
 			p2: p2
 		},
-		dataType: "json",
+		//dataType: "json",
 		type: "POST",
 		timeout:  6000,
 		success: function(json) {
-			//console.log(json);
-			//return false;
+			console.log(json);
+			return false;
 			$("#btn_login").attr("disabled", false);
 			for (var i = 0; i < json.length; i++) {             
 				if(json[i].result == "true"){
