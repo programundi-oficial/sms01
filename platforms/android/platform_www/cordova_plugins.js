66a3acc534bc3819ed798737e1efaf861e055aaf
unknown <rodrigo@sicapsolucoes.com.br>
2019-05-27 14:35:24 -0300

    sms001

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-sms.SMS",
+    "file": "plugins/cordova-plugin-sms/www/SMS.js",
+    "pluginId": "cordova-plugin-sms",
+    "clobbers": [
+      "window.SMS"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-sms": "1.0.5"
+};
+// BOTTOM OF METADATA
+});
--- www/important.js
+++
@@ -1,1 +0,0 @@
-var xurlq 		= "https://sicapsolucoes.com.br/teste_joyce/servicos.php";
